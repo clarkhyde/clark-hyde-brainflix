d983c1c50a3f867803835cf915d9dabdbc4056ff
Clark <clarkhyde123@gmail.com>
2021-02-26 10:03:11 -0600

    actually fixed some sizing stuff in nav

--- BrainFlix-Sprint-1/clark-hyde-brainflix/src/components/Nav.js
+++ BrainFlix-Sprint-1/clark-hyde-brainflix/src/components/Nav.js
@@ -10,10 +10,10 @@ function Nav()  {
                 <img src={logo} className = "nav__image" alt="Brainflix"/>
                 <ul className = "nav-right">
                     <li className = "nav-right__search">
-                        <input type="text" className ="nav-right__input" placeholder= "     Search"></input>
+                        <input type="text" className ="nav-right__input" placeholder= "           Search"></input>
                     </li>
                     <li className = "nav-right__upload">
-                        <a href = "#"><img src={plus}></img>UPLOAD</a>
+                        <a href = "#"><img src={plus}></img>   UPLOAD</a>
                     </li>
                     <li className = "nav-right__profile">
                         <img src={profileImage} className = "nav-right__image"></img>
